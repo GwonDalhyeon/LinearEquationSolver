10fcdde48c1df041cc2566fdeb54e140c6e484f0
GwonDalhyeon <pygmalion85@gmail.com>
2015-07-02 14:05:25 +0900

    Add vector type matix solver
    Origin solver only act on a double pointer A.
    But one can use a vector type(A[i*colNum+j]) matrix A.

--- main.cpp
+++ main.cpp
@@ -3,33 +3,30 @@
 void main()
 {
 	int num = 4;
-	double** A = new double*[num];
+	double* A = new double[num*num];
 	double* b = new double[num];
 	double* x = new double[num];
-	for (int i = 0; i < num; i++)
-	{
-		A[i] = new double[num];
-	}
-	A[0][0] = 4;
-	A[0][1] = 3;
-	A[0][2] = 2;
-	A[0][3] = 1;
 
-	A[1][0] = 3;
-	A[1][1] = 3;
+	A[0*num + 0] = 4;
+	A[0*num + 1] = 3;
+	A[0*num + 2] = 2;
+	A[0*num + 3] = 1;
 
-	A[1][2] = 2;
-	A[1][3] = 1;
+	A[1*num + 0] = 3;
+	A[1*num + 1] = 3;
+
+	A[1*num + 2] = 2;
+	A[1*num + 3] = 1;
 	
-	A[2][0] = 2;
-	A[2][1] = 2;
-	A[2][2] = 2;
-	A[2][3] = 1;
+	A[2*num + 0] = 2;
+	A[2*num + 1] = 2;
+	A[2*num + 2] = 2;
+	A[2*num + 3] = 1;
 	
-	A[3][0] = 1;
-	A[3][1] = 1;
-	A[3][2] = 1;
-	A[3][3] = 1;
+	A[3*num + 0] = 1;
+	A[3*num + 1] = 1;
+	A[3*num + 2] = 1;
+	A[3*num + 3] = 1;
 
 	b[0] = 16, b[1] = 17;
 	b[2] = 15, b[3] = 9;
@@ -38,7 +35,7 @@ void main()
 
 	for (int i = 0; i < num; i++)
 	{
-		delete A[i];
+		cout<<x[i]<<endl;
 	}
 	delete A, b, x;
 }
--- operators.h
+++ operators.h
@@ -6,6 +6,7 @@ using namespace std;
 
 
 
+
 double* CG(int num, double** A, double* b)
 {
 	double tolerance = 10*DBL_EPSILON;
@@ -91,6 +92,90 @@ double* CG(int num, double** A, double* b)
 }
 
 
+double* CG(int num, double* A, double* b)
+{
+	double tolerance = 10*DBL_EPSILON;
+	double* rOld = new double[num];
+	double* p = new double[num];
+	double* rNew = new double[num];
+	double* x = new double[num];
+
+	for (int i = 0; i < num; i++)
+	{
+		rOld[i] = b[i];
+		p[i] = b[i];
+		x[i] = 0;
+	}
+
+	double alpha = 0;
+	double beta = 0;
+	double temp1 = 0, temp2 = 0;
+	double temp = 0;
+	double residual;
+
+	for (int k = 0; k < 1000; k++)
+	{
+		temp1 = 0;
+		temp2 = 0;
+		for (int i = 0; i < num; i++)
+		{
+			temp1 = temp1 + rOld[i]*rOld[i];
+			for (int j = 0; j < num; j++)
+			{
+				temp2 = temp2 + p[i]*A[i*num + j]*p[j];
+			}
+		}
+		alpha = temp1/temp2;
+	
+		for (int i = 0; i < num; i++)
+		{
+			x[i] = x[i] +alpha*p[i];
+			temp = 0;
+			for (int j = 0; j < num; j++)
+			{
+				temp = temp + A[i*num + j]*p[j];
+			}
+			rNew[i] = rOld[i] - alpha*temp;
+		}
+
+		residual = 0;
+		for (int i = 0; i < num; i++)
+		{
+			residual = residual + rNew[i]*rNew[i];
+		}
+		residual = sqrt(residual);
+
+		if (residual < tolerance)
+		{
+			delete rNew, rOld, p;
+			return x;
+		}
+
+		temp1 = 0;
+		temp2 = 0;
+		for (int i = 0; i < num; i++)
+		{
+			temp1 = temp1 + rNew[i]*rNew[i];
+			temp2 = temp2 + rOld[i]*rOld[i];
+		}
+		beta = temp1/temp2;
+
+		for (int i = 0; i < num; i++)
+		{
+			p[i] = rNew[i] + beta*p[i];
+		}
+	
+		for (int i = 0; i < num; i++)
+		{
+			rOld[i] = rNew[i];
+		}
+
+	}
+	delete rNew, rOld, p;
+	return x;
+	
+}
+
 double** incompleteCholesky(int num, double** A)
 {
 	double** L;
@@ -127,6 +212,47 @@ double** incompleteCholesky(int num, double** A)
 					temp = temp + L[i][k]*L[j][k];
 				}
 				L[j][i] = (A[i][j] - temp)/L[i][i];
+			}
+		}
+	}
+
+	return L;
+}
+
+double* incompleteCholesky(int num, double* A)
+{
+	double* L= new double[num*num];
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+		{
+			L[i*num + j] = 0;
+		}
+	}
+
+	double temp;
+	for (int i = 0; i < num; i++)
+	{
+		temp = 0;
+		if (A[i*num + i]!=0)
+		{
+			for (int j = 0; j <= i-1; j++)
+			{
+				temp = temp + L[i*num + j]*L[i*num + j];
+			}
+			L[i*num + i] = sqrt(A[i*num + i] - temp);
+		}
+		
+		for (int j = i+1; j < num; j++)
+		{
+			if (A[i*num + j]!=0 && L[i*num + i]!=0)
+			{
+				temp = 0;
+				for (int k = 0; k <= i-1; k++)
+				{
+					temp = temp + L[i*num + k]*L[j*num + k];
+				}
+				L[j*num + i] = (A[i*num + j] - temp)/L[i*num + i];
 			}
 		}
 	}
@@ -268,4 +394,126 @@ double* PCG(int num, double** A, double* b)
 	
 }
 
+double* PCG(int num, double* A, double* b)
+{
+	double tolerance = 10*DBL_EPSILON;
+	double* rOld = new double[num];
+	double* p = new double[num];
+	double* rNew = new double[num];
+	double* zOld = new double[num];
+	double* zNew = new double[num];
+	double* x = new double[num];
+
+	double* L = incompleteCholesky(num,A);
+	double* M = new double[num*num];
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+		{
+			M[i*num + j]  = 0;
+			for (int k = 0; k < num; k++)
+			{
+				M[i*num + j] = M[i*num + j] + L[i*num + k]*L[j*num + k];
+			}
+		}
+	}
+
+	double temp = 0;
+	for (int i = 0; i < num; i++)
+	{
+		temp = 0;
+		for (int j = 0; j < num; j++)
+		{
+			temp = temp + M[j*num + i]*rOld[j];
+		}
+		zOld[i] = temp;
+		p[i] = zOld[i];
+		rOld[i] = b[i];
+		x[i] = 0;
+
+	}
+
+	double alpha = 0;
+	double beta = 0;
+	double temp1 = 0, temp2 = 0;
+	double residual;
+
+	for (int k = 0; k < 1000; k++)
+	{
+		temp1 = 0;
+		temp2 = 0;
+		for (int i = 0; i < num; i++)
+		{
+			temp1 = temp1 + rOld[i]*zOld[i];
+			for (int j = 0; j < num; j++)
+			{
+				temp2 = temp2 + p[i]*A[i*num + j]*p[j];
+			}
+		}
+		alpha = temp1/temp2;
+	
+		for (int i = 0; i < num; i++)
+		{
+			x[i] = x[i] +alpha*p[i];
+			temp = 0;
+			for (int j = 0; j < num; j++)
+			{
+				temp = temp + A[i*num + j]*p[j];
+			}
+			rNew[i] = rOld[i] - alpha*temp;
+		}
+
+		residual = 0;
+		for (int i = 0; i < num; i++)
+		{
+			residual = residual + rNew[i]*rNew[i];
+		}
+		residual = sqrt(residual);
+
+		if (residual < tolerance)
+		{
+			delete zNew, zOld, rNew, rOld, p;
+			delete M,L;
+
+			return x;
+		}
+
+		for (int i = 0; i < num; i++)
+		{
+			temp = 0;
+			for (int j = 0; j < num; j++)
+			{
+				temp = temp + M[j*num + i]*rNew[j];
+			}
+			zNew[i] = temp;
+		}
+
+		temp1 = 0;
+		temp2 = 0;
+		for (int i = 0; i < num; i++)
+		{
+			temp1 = temp1 + rNew[i]*zNew[i];
+			temp2 = temp2 + rOld[i]*zOld[i];
+		}
+		beta = temp1/temp2;
+
+		for (int i = 0; i < num; i++)
+		{
+			p[i] = zNew[i] + beta*p[i];
+		}
+	
+		for (int i = 0; i < num; i++)
+		{
+			rOld[i] = rNew[i];
+			zOld[i] = zNew[i];
+		}
+
+	}
+
+	delete zNew, zOld, rNew, rOld, p;
+	delete M,L;
+
+	return x;
+	
+}
 
